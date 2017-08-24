fee70273fc4a326ba114ec79f69b22297ca0ae3b
UniCesumar <UniCesumar@D46XHJ82>
2017-08-23 22:40:57 -0300

    Terceira Versão

--- WebContent/listCustomer.jsp
+++ WebContent/listCustomer.jsp
@@ -1,13 +1,22 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>List Customer</title>
+<title>Listar Clientes</title>
 </head>
 <body>
-	<h1>List Customer</h1>
-	<hr>
+<h1>Lista de Clientes</h1>
+<hr>
+<c:choose>
+	<c:when test="${not empty listCustomer}">
+	Lista cheia
+	</c:when>
+	<c:otherwise>
+	Lista vazia
+	</c:otherwise>
+</c:choose>
 </body>
 </html>
---
+++ src/br/edu/unicesumar/servlets/ListCustomerServlet.java
@@ -0,0 +1,55 @@
+package br.edu.unicesumar.servlets;
+
+import br.edu.unicesumar.model.Customer;
+import br.edu.unicesumar.service.CustomerService;
+
+import java.util.ArrayList;
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListCustomerServlet
+ */
+@WebServlet("/ListCustomerServlet")
+public class ListCustomerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListCustomerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		CustomerService service = new CustomerService();
+				
+		ArrayList<Customer> list = new ArrayList<Customer>();
+		
+		list = (ArrayList<Customer>) service.listAll();
+		
+		request.setAttribute("listCustomer",list);
+		
+		RequestDispatcher rd = getServletContext().getRequestDispatcher("/listCustomer.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
